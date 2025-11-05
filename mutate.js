236e61616545c45ecbe771dea32e4d22da935305
Gian Peña <flaconumero2@gmail.com>
2025-11-05 00:59:14 -0500

    remove these as I have no use for them right now

--- mutate.js
+++
@@ -1,4 +0,0 @@
-import { watchEpisode } from "./WatchEpisode.js";
-
-// leave this as an example for now
-watchEpisode(153800, 4);
--- watching.js
+++
@@ -1,6 +0,0 @@
-import { getCurrentlyWatching } from "./CurrentlyWatching.js";
-
-getCurrentlyWatching().then(data => {
-  const { watching } = data;
-  console.log(watching);
-});
